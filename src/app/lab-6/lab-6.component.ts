de3965ba2631eef7f99fa2fb0c47accef2f30acf
alem2coder1 <148032941+alem2coder1@users.noreply.github.com>
2024-03-14 22:40:19 +0500

    lab/-6

---
+++ src/app/lab-6/lab-6.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-6',
+  standalone: true,
+  imports: [],
+  templateUrl: './lab-6.component.html',
+  styleUrl: './lab-6.component.css'
+})
+export class Lab6Component {
+
+}
